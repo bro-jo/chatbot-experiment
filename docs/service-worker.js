f243ce6b8bb42ebc0bec0e04bacba9cc59107e1b
조형재 <jhj12010@gmail.com>
2020-11-03 20:38:58 +0900

    -

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.30077dd504273b272ce5c5a395cbb0d7.js"
+  "/precache-manifest.a8469e7ac7f4d6203fed77387616b386.js"
 );
 
 self.addEventListener('message', (event) => {
